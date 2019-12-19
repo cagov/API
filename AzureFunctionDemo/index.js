c51740d0d88f67ccc32edb7def9a01d72a7b8f69
carterm <carter.medlin@gmail.com>
2019-12-19 12:35:42 -0800

    code change test

--- AzureFunctionDemo/index.js
+++ AzureFunctionDemo/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {``
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello NAME2 =  " + (req.query.name || req.body.name)
+            body: "Hello NAME3 =  " + (req.query.name || req.body.name)
         };
     }
     else {
