f20ad35dade54ab435d641d9bb8bc876bd6b9e29
carterm <carter.medlin@gmail.com>
2019-12-20 15:04:37 -0800

    Removing unused dependencies

--- AzureFunctionDemo/index.js
+++ AzureFunctionDemo/index.js
@@ -5,31 +5,9 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('Query = ' + JSON.stringify(req.query));
 
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-    //         body: "V7 -   Hello NAME =  " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: "Please pass a name on the query string or in the request body"
-    //     };
-    // }
-
     const https = require("https");
     https.get(icalUrl, response => {
-        
-        // if (!response.error && response.statusCode == 200) {
-        //     context.log('request complete');
-
-        //     res = { isRaw: true, body: JSON.stringify(response.body)};
-        //     context.res = res;
-        // } else {
-        //     context.log('request PROBLEM')
-        // }
-
+  
         var body = '';
         var i = 0;
         response.on('data', function (chunk) {
@@ -48,7 +26,6 @@ module.exports = function (context, req) {
                 }
             };
             context.res = res;
-            //context.body.send(body);
 
             console.log('Finished');
             context.done();
@@ -56,21 +33,4 @@ module.exports = function (context, req) {
 
 
       });
-
-
-    // var request = require('request');
-    // context.log('request start');
-    // request.get(icalUrl, function (error, response, body) {
-    //     if (!error && response.statusCode == 200) {
-    //         context.log('request complete');
-
-    //         res = { isRaw: true, body: JSON.stringify(body)};
-    //         context.res = res;
-    //     } else {
-    //         context.log('request PROBLEM')
-    //     }
-    //     context.log('*****************************************************');
-    //     context.done();
-
-    // });
 };
