89fca087871a29ce426e2233d55c158589a0e014
carterm <carter.medlin@gmail.com>
2019-12-20 14:57:36 -0800

    Returns a file as an attachment

--- AzureFunctionDemo/index.js
+++ AzureFunctionDemo/index.js
@@ -18,20 +18,59 @@ module.exports = function (context, req) {
     //     };
     // }
 
+    const https = require("https");
+    https.get(icalUrl, response => {
+        
+        // if (!response.error && response.statusCode == 200) {
+        //     context.log('request complete');
 
-    var request = require('request');
-    context.log('request start');
-    request.get(icalUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            context.log('request complete');
+        //     res = { isRaw: true, body: JSON.stringify(response.body)};
+        //     context.res = res;
+        // } else {
+        //     context.log('request PROBLEM')
+        // }
 
-            res = { isRaw: true, body: JSON.stringify(body)};
+        var body = '';
+        var i = 0;
+        response.on('data', function (chunk) {
+            i++;
+            body += chunk;
+            console.log('BODY Part: ' + i);
+        });
+        response.on('end', function () {
+    
+            res = { 
+                isRaw: 'true', 
+                body: body,
+                headers: {
+                    'Content-Disposition' : 'attachment; filename="California State Holidays.ics"',
+                    'Content-Type' : 'text/calendar'
+                }
+            };
             context.res = res;
-        } else {
-            context.log('request PROBLEM')
-        }
-        context.log('*****************************************************');
-        context.done();
+            //context.body.send(body);
 
-    });
+            console.log('Finished');
+            context.done();
+        });
+
+
+      });
+
+
+    // var request = require('request');
+    // context.log('request start');
+    // request.get(icalUrl, function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         context.log('request complete');
+
+    //         res = { isRaw: true, body: JSON.stringify(body)};
+    //         context.res = res;
+    //     } else {
+    //         context.log('request PROBLEM')
+    //     }
+    //     context.log('*****************************************************');
+    //     context.done();
+
+    // });
 };
