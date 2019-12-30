7409f68f83efc4a9696de1e4cc77fb7560b84401
carterm <carter.medlin@gmail.com>
2019-12-30 09:43:16 -0800

    Adding a "ConnectionTest" sample

---
+++ ConnectionTest/index.js
@@ -0,0 +1,9 @@
+module.exports = async function (context, req) {
+   
+//Leave this as is for a template to use for new services
+
+        context.res = {
+            body: "Connection Success!"
+        };
+
+};
