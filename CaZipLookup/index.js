c3d9c6ff75581844545d2f1ebb45a2a6fb9850ab
carterm <carter.medlin@gmail.com>
2020-01-17 16:07:07 -0800

    service works

--- CaZipLookup/index.js
+++ CaZipLookup/index.js
@@ -1,50 +1,42 @@
 module.exports = async function (context, req) {
     const fs = require('fs');
 
-//Leave this as is for a template to use for new services
+    let input = context.req.params.zip;
 
+    if (input) {
+        let zips = JSON.parse(fs.readFileSync(context.executionContext.functionName + '/data.json','utf8'));
+        let out = [];
+        zips.forEach(item => {
+            var zip = Object.keys(item)[0];
+            if(zip === input) {
+                let cities = item[zip];
+                let cityout = [];
 
-        if (context.req.params.zip) {
-            //let rawdata = fs.readFileSync(context.executionContext.functionName + '/data.json');
-            let zips = JSON.parse(fs.readFileSync(context.executionContext.functionName + '/data.json','utf8'));
-            
-            let out = [];
-            zips.forEach( (item) => {
-                for(var key in item) {
-                    if(key == context.req.params.zip) {
-                        let cities = item[key];
-                        let cityout = [];
+                for(var key2 in cities)
+                    cityout.push({"name":cities[key2]});
 
-                        for(var key2 in cities) {
-                            cityout.push({"name":cities[key2]});
-                        }
-
-
-
-                    //out.push({"zip":parseInt(context.req.params.zip,10), "city":item[key]});
-                    out.push({"zip":context.req.params.zip, "cities":cityout});
-                        //out = item[key];
-                    }
-                }
-              });
-            
-
-
-
-              
+                out.push({"zip":zip, "cities":cityout});
+            }
+            });
+        
+        if(out.length==0) 
             context.res = {
-                
-
-
-
                 // status: 200, /* Defaults to 200 */
-                body: out
+                status: 404,
+                body: "Zip not found - " + input
             };
-        }
-        else {
+        else
             context.res = {
-                status: 400,
-                body: "Please pass a zip on the path"
+                // status: 200, /* Defaults to 200 */
+                body: out,
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
             };
-        }
+    }
+    else 
+        context.res = {
+            status: 400,
+            body: "Please pass a zip on the path"
+        };
 };
