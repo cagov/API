8a45838271c149f147ebd724523a97a1f7a881a1
carterm <carter.medlin@gmail.com>
2020-02-05 15:59:16 -0800

    removed lane closures for now

--- StateHolidayCalendar/index.js
+++ StateHolidayCalendar/index.js
@@ -1,19 +1,21 @@
 module.exports = function (context, req) {
-    const icalUrl = 'https://calendar.google.com/calendar/ical/alpha.ca.gov_m9372uj21m6qdgrqg4olkdgo4c%40group.calendar.google.com/public/basic.ics';
+    const icalUrl = 'https://calendar.google.com/calendar/ical/alpha.ca.gov_m9372uj21m6qdgrqg4olkdgo4c%40group.calendar.google.com/public/basic.ics'
 
     //context.log('*****************************************************');
     //context.log('JavaScript HTTP trigger function processed a request.');
     //context.log('Query = ' + JSON.stringify(req.query));
 
-    const https = require('https');
+switch ((context.req.params.route || "").toLowerCase()) {
+case '.ics':
+    const https = require('https')
     https.get(icalUrl, response => {
-  
-        var body = '';
-        var i = 0;
+
+        var body = ''
+        var i = 0
         response.on('data', function (chunk) {
             i++;
             body += chunk;
-        });
+        })
         response.on('end', function () {
             res = { 
                 isRaw: 'true', 
@@ -22,11 +24,25 @@ module.exports = function (context, req) {
                     'Content-Disposition' : 'attachment; filename="California State Holidays.ics"',
                     'Content-Type' : 'text/calendar'
                 }
-            };
-            context.res = res;
+            }
+            context.res = res
 
-            //console.log('Finished');
-            context.done();
-        });
-      });
-};
+            //console.log('Finished')
+            context.done()
+        })
+    })
+    break
+case 'next':
+    context.res.body = 'next time'
+    context.done()
+    break
+default:
+    context.res.body = 'nevermind'
+    context.done()
+}
+
+
+
+
+
+}
