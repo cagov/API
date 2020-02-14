c958b606b94429608a2f9ef8c68c278681726577
carterm <carter.medlin@gmail.com>
2020-02-14 15:19:17 -0800

    code improvements...adding url

--- HomelessShelters/index.js
+++ HomelessShelters/index.js
@@ -2,6 +2,7 @@ const dataurl = "https://www.shelterlistings.org/city/"
 const pagestartmarker = "Search listings by <a href=\""
 const jsonstartmarker = /<script type="application\/ld\+json">/
 const jsonendmarker = /<\/script>/
+const linkpattern = /https:\/\/www.shelterlistings.org\/details\/\d+\//
 const soft404page = "https://www.shelterlistings.org/nocity.html"
 
 module.exports = async function (context, req) {
@@ -27,25 +28,38 @@ module.exports = async function (context, req) {
             }
         else {
             //looking for these markers...<script type="application/ld+json">
-            const fullhtml = await response.text()
-            const html = fullhtml.split(pagestartmarker,2)[1]
+            const html = (await response.text()).split(pagestartmarker,2)[1]
 
-            const jsondataset = html
+            let results = []
+
+            html
                 .split(jsonstartmarker)
-                .map(
-                    (value,i)=>
-                        i === 0
-                        ? null
-                        : JSONlogparse(
-                            value
-                            .split(jsonendmarker,1)[0]
+                .forEach((value,i) => {
+                    if(i > 0) {
+                        const jsonsplit = value.split(jsonendmarker,2)
+                        const url = jsonsplit[1].match(linkpattern)[0]
+
+                        let x = JSONlogparse(
+                            jsonsplit[0]
                             .replace(/(\n|\r|\t)/g,'')
                             )
-                    )
+
+                        results.push({
+                                "name" : x.name,
+                                "address" : x.address.streetAddress,
+                                "city" : x.address.addressLocality,
+                                "state" : x.address.addressRegion,
+                                "zipcode" : x.address.postalCode,
+                                "phone" : x.telephone,
+                                url,
+                                "description" : x.description
+                            })
+                    }
+                })
 
             context.res = 
                 {
-                    body: jsondataset,
+                    body: results,
                     headers: {
                         'Content-Type' : 'application/json'
                     }
