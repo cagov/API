7a8dba9d5e5d26c977334c274245d89e235d44b4
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-02-18 18:19:05 -0800

    new endpoint

---
+++ CountyFromZip/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const zips = JSON.parse(fs.readFileSync('CaZipLookup/counties.json','utf8'));
+
+module.exports = async function (context, req) {
+    const input = context.req.params.zip;
+
+    if (input) {
+        let cities = [];
+        for (const ziprow of zips) {
+            cities = ziprow[input];
+            if (cities) break;
+        }
+
+        if (cities) 
+            context.res = {
+                body: {"zip":input, "cities":cities.map(cityname => ({"name" : cityname} ))},
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
+            };
+        else 
+            context.res = {
+                status: 404,
+                body: "Zip not found - " + input
+            };
+    } else 
+        context.res = {
+            status: 400,
+            body: "Please pass a zip on the path"
+        };
+};
