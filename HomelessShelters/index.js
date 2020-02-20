d01788ee8aae235e9a11ecb72b9c71cf46b6768e
carterm <carter.medlin@gmail.com>
2020-02-20 13:13:35 -0800

    adding match to result, and 404 for no result, bounding box

--- HomelessShelters/index.js
+++ HomelessShelters/index.js
@@ -9,7 +9,8 @@ module.exports = async function (context, req) {
     if (query) {
         const point = await geocode(query)
 
-        const coords = data
+        if(point) {
+            const coords = data
             .map((x,i) => {
                 return {
                     i,
@@ -18,33 +19,38 @@ module.exports = async function (context, req) {
                 }
             })
         
-        const results = geolib
-            .orderByDistance(point, coords)
-            .slice(0,10)
-            .map(x=>data[x.i])
+            const results = geolib
+                .orderByDistance(point, coords)
+                .slice(0,10)
+                .map(x=>data[x.i])
 
-        results.forEach(x=> 
-            x.location["distance"] =
-                round(
-                    geolib.convertDistance(
-                        geolib.getDistance(
-                            {
-                                latitude:x.location.lat,
-                                longitude:x.location.lon
-                            }
-                        , point)
-                    ,'mi')
-                ,2)
-        )
+            results.forEach(x=> 
+                x.location["distance"] =
+                    round(
+                        geolib.convertDistance(
+                            geolib.getDistance(
+                                {
+                                    latitude:x.location.lat,
+                                    longitude:x.location.lon
+                                }
+                            , point)
+                        ,'mi')
+                    ,2)
+            )
 
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: results,
-            headers: {
-                'Content-Type' : 'application/json',
-                "Cache-Control" : "public, max-age=84600" //1 day
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: {match: point, results},
+                headers: {
+                    'Content-Type' : 'application/json',
+                    "Cache-Control" : "public, max-age=84600" //1 day
+                }
             }
-        }
+        } else 
+            context.res = {
+                status: 404,
+                body: "No result for query - "+query
+            }
     }
     else 
         context.res = {
@@ -72,15 +78,15 @@ async function geocode(query) {
     if(!apikey)
         throw Error("Api key (FUNCTIONS_MAP_KEY) is missing from configuration.  See source comment for help.")
 
-    const geolink = `https://atlas.microsoft.com/search/address/json?subscription-key=${apikey}&api-version=1.0&limit=1&countrySet=US&extendedPostalCodesFor=None&query=`
+    const geolink = `https://atlas.microsoft.com/search/address/json?subscription-key=${apikey}&api-version=1.0&limit=1&countrySet=US&extendedPostalCodesFor=None&topLeft=42,-124.5&btmRight=32.5,-114.1&query=`
 // Getcoder docs - https://docs.microsoft.com/en-us/rest/api/maps/search/getsearchaddress
 
     const georesponse =  await fetch(geolink+encodeURIComponent(query))
     if (georesponse.ok) {
         const geojson = await georesponse.json()
-        const georesult =  geojson.summary.numResults>0 ? geojson.results[0].position : null
+        const georesult =  geojson.summary.numResults>0 ? geojson.results[0] : null
         if(georesult)
-            return {latitude: georesult.lat, longitude:georesult.lon}
+            return {latitude: georesult.position.lat, longitude:georesult.position.lon, match: georesult.address.freeformAddress}
     }
 }
 
