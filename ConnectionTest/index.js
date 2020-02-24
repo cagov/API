0b3bcc049e00ef0a88ab2a23dcbd76cbd5bd71fe
carterm <carter.medlin@gmail.com>
2020-02-24 15:45:25 -0800

    Restoring connection test

--- ConnectionTest/index.js
+++ ConnectionTest/index.js
@@ -1,20 +1,15 @@
-var data = []
+//var data = []
 
 module.exports = async function (context, req) {
    
 //Leave this as is for a template to use for new services
 
-const now = new Date().toLocaleString('en-US', {
-    //timeZoneName: 'short'
-
-    timeZone: "America/Los_Angeles"
-    //timeZone: 'Pacific Standard Time'
-  });
-
-data.push({"date":now, "ip":req.headers['x-forwarded-for']})
+//const now = new Date().toLocaleString('en-US', {timeZone: "America/Los_Angeles"});
+//data.push({"date":now, "ip":req.headers['x-forwarded-for']})
 
         context.res = {
-            body: "Connection Success! - \n\n" + JSON.stringify(data,null, '  ')
+            //body: "Connection Success! - \n\n" + JSON.stringify(data,null, '  ')
+            body: "Connection Success!"
         };
 
 };
