9682527f621d1e3837c4c230c61420ef3178af66
carterm <carter.medlin@gmail.com>
2020-02-24 14:59:47 -0800

    Experiment with service

--- ConnectionTest/index.js
+++ ConnectionTest/index.js
@@ -1,9 +1,15 @@
+var data = []
+
 module.exports = async function (context, req) {
    
 //Leave this as is for a template to use for new services
 
+const now = new Date()
+
+data.push({"date":now.toLocaleTimeString(), "ip":req.headers['x-forwarded-for']})
+
         context.res = {
-            body: "Connection Success!"
+            body: "Connection Success! - " + JSON.stringify(data)
         };
 
 };
