a316a58b67d3d62fcea0250cac22237b29805fd0
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-07-14 16:24:43 -0700

    update to use new cosmos

--- WasHelpful/index.js
+++ WasHelpful/index.js
@@ -1,23 +1,35 @@
 const CosmosClient = require('@azure/cosmos').CosmosClient
-const config = require('./config')
+let config = require('./config.js')
 const endpoint = config.endpoint
 const key = config.key
+const databaseId = config.database.id
+const containerId = config.container.id
 const client = new CosmosClient({ endpoint, key })
 
 module.exports = async function (context, req) {
+    const myURL = new URL(req.body.url);
+    let site = myURL.hostname;
+    let helpful = parseBoolean(req.body.helpful);
+    let comments = req.body.comments;
     let timestamp = new Date().getTime()
     let insertBody = {
+        id: `${site}-${timestamp}-${Math.random()}`,
         time: timestamp,
-        site: req.body.site,
-        url: req.body.url,
-        helpful: parseBoolean(req.body.helpful),
-        comments: req.body.comments
+        site: site,
+        url: myURL.href,
+        helpful: helpful,
+        comments: comments
     }
 
-    const { item } = await client
-        .database(config.database.id)
-        .container(config.container.id)
-        .items.upsert(insertBody)
+    async function createItem(itemBody) {
+        const { item } = await client
+            .database(databaseId)
+            .container(containerId)
+            .items.upsert(itemBody)
+        return({"message":`Created family item with id: ${itemBody.id}`})
+    }
+
+    let item = await createItem(insertBody)
 
     context.res = {
         headers: {
@@ -28,3 +40,4 @@ module.exports = async function (context, req) {
 }
 
 
+  
