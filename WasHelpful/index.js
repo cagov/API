d14ec915fe7ec3af13fd7dad3acc9bdb0d1b89b4
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-07-14 17:38:52 -0700

    parse boolean is not a thing

--- WasHelpful/index.js
+++ WasHelpful/index.js
@@ -7,36 +7,42 @@ const containerId = config.container.id
 const client = new CosmosClient({ endpoint, key })
 
 module.exports = async function (context, req) {
-    const myURL = new URL(req.body.url);
-    let site = myURL.hostname;
-    let helpful = parseBoolean(req.body.helpful);
-    let comments = req.body.comments;
-    let timestamp = new Date().getTime()
-    let insertBody = {
-        id: `${site}-${timestamp}-${Math.random()}`,
-        time: timestamp,
-        site: site,
-        url: myURL.href,
-        helpful: helpful,
-        comments: comments
+    if(req.body) {
+        const myURL = new URL(req.body.url);
+        let site = myURL.hostname;
+        let helpful = req.body.helpful === 'true';
+        let comments = req.body.comments;
+        let timestamp = new Date().getTime()
+        let insertBody = {
+            id: `${site}-${timestamp}-${Math.random()}`,
+            time: timestamp,
+            site: site,
+            url: myURL.href,
+            helpful: helpful,
+            comments: comments
+        }
+    
+        async function createItem(itemBody) {
+            const { item } = await client
+                .database(databaseId)
+                .container(containerId)
+                .items.upsert(itemBody)
+            return({"message":`Created family item with id: ${itemBody.id}`})
+        }
+    
+        let item = await createItem(insertBody)
+    
+        context.res = {
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: item
+        };
+    } else {
+        context.res = {
+            body: 'Hi'
+        };
     }
-
-    async function createItem(itemBody) {
-        const { item } = await client
-            .database(databaseId)
-            .container(containerId)
-            .items.upsert(itemBody)
-        return({"message":`Created family item with id: ${itemBody.id}`})
-    }
-
-    let item = await createItem(insertBody)
-
-    context.res = {
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: item
-    };
 }
 
 
