08e1457375baf0437ebd8e02e1f986fb24d60cf4
Aaron Hans <aaronhans@gmail.com>
2021-07-16 17:26:03 -0700

    changes requested by data team

--- FeedbackData/config.js
+++ FeedbackData/config.js
@@ -1,6 +1,7 @@
 let config = {}
 config.endpoint = process.env["COSMOS_ENDPOINT_FEEDBACK"];
 config.key = process.env["COSMOS_KEY_FEEDBACK"];
+config.requestor = process.env["FEEDBACK_REQUESTOR"]
 config.database = {
   id: 'Feedback'
 }
--- FeedbackData/index.js
+++ FeedbackData/index.js
@@ -9,12 +9,11 @@ const client = new CosmosClient({ endpoint, key });
 module.exports = async function (context, req) {
   async function queryContainer() {
     let startDate = new Date().getTime() - (1000 * 90 * 24 * 60 * 60);
-    // remove some fields
     let querySpec = {
       query: `SELECT * FROM c WHERE c.time >= ${startDate}`,
       parameters: [],
     };
-    if (context.req.query.url) {
+    if (context.req.query.url && req.query.requestor === config.requestor) {
       querySpec = {
         query: `SELECT * FROM c WHERE c.time >= ${startDate} AND CONTAINS(c.url, "${decodeURIComponent(context.req.query.url)}")`,
         parameters: [],
@@ -28,16 +27,22 @@ module.exports = async function (context, req) {
       .container(containerId)
       .items.query(querySpec)
       .fetchAll();
-    for (var queryResult of results) {
-      let resultString = JSON.stringify(queryResult);
-      console.log(`\tQuery returned ${resultString}\n`);
-    }
-    return results;
+    return results.map(item => {
+      // reorganizing object per data team's specs
+      delete item._rid;
+      delete item._self;
+      delete item._etag;
+      delete item._attachments;
+      delete item.time;
+      item.helpful = (item.helpful ? "yes" : "no");
+      item.timestamp = item._ts;
+      delete item._ts;
+      return item;
+    }).sort((a, b) => a - b);
   }
 
   let responseMessage = await queryContainer();
   context.res = {
-    // status: 200, /* Defaults to 200 */
     body: responseMessage,
   };
 };
