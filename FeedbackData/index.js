afa7cd3017ecf5ec24b0f1bf9bc0ff79648235ea
Aaron Hans <aaronhans@gmail.com>
2021-07-24 08:48:20 -0700

    fix nulls, put date back in

--- FeedbackData/index.js
+++ FeedbackData/index.js
@@ -31,8 +31,7 @@ module.exports = async function (context, req) {
     return results.map(item => {
       let u = new URL(item.url);
       item.page = u.pathname;
-      let re = new RegExp('/#.+');
-      item.page_section = re.exec(u);
+      item.page_section = u.hash;
       // there are no translated language urls on cannabis or drought
       item.helpful = (item.helpful ? "yes" : "no");
       let date_obj = new Date(item.time);
@@ -41,13 +40,13 @@ module.exports = async function (context, req) {
       // current month
       let month = ("0" + (date_obj.getMonth() + 1)).slice(-2);
 
-      item.timestamp = date_obj.getFullYear() + "-" + month + "-" + date; //  + " " + date_obj.getHours() + ":" + date_obj.getMinutes() + ":" + date_obj.getSeconds()
+      item.timestamp = item._ts; // date_obj.getFullYear() + "-" + month + "-" + date + " " + date_obj.getHours() + ":" + date_obj.getMinutes() + ":" + date_obj.getSeconds();
       delete item._attachments;
       delete item._etag;
       delete item._rid;
       delete item._self;
       delete item._ts;
-      delete item.time;
+      // delete item.time;
       return item;
     }).sort((a, b) => a - b);
   }
